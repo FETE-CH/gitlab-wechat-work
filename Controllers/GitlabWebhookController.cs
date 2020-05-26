74a6ba8b3a603dbdce329a6797b36465532f552b
FETE <175717629@qq.com>
2020-05-26 16:40:49 +0800

    init repo

---
+++ Controllers/GitlabWebhookController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace wechat_robot.Controllers {
+    public class GitlabWebhookControllers : Controller {
+        // GET
+        public IActionResult Index() {
+            return null;
+        }
+    }
+}
